cd2d8b0f424ee1e36f3f822afbf296b6c642b0f4
ohjungmin <jmin980317@naver.com>
2023-08-16 18:59:55 +0900

    add route input -> choice

--- lib/screen/input.dart
+++ lib/screen/input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:SKT_FLY_AI/screen/choice.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,7 +22,9 @@ class InputScreen extends StatefulWidget {
 }
 
 class _InputScreenState extends State<InputScreen> {
-  int friendsCount = 0; // 초기 값 설정
+  int friendsCount = 0;
+  bool isAloneSelected = false;
+  bool isParticipateSelected = false;
 
   void increaseFriendsCount() {
     setState(() {
@@ -35,6 +38,13 @@ class _InputScreenState extends State<InputScreen> {
         friendsCount--;
       }
     });
+  }
+
+  void navigateToChoiceScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ChoiceScreen()),
+    );
   }
 
   @override
@@ -59,7 +69,7 @@ class _InputScreenState extends State<InputScreen> {
       backgroundColor: Color(0xffF1F2F6),
       body: Column(
         children: [
-          SizedBox(height: 90), // Set spacing between AppBar and buttons
+          SizedBox(height: 90),
           Center(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -68,9 +78,15 @@ class _InputScreenState extends State<InputScreen> {
                   height: 102,
                   width: 171.48,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        isAloneSelected = !isAloneSelected;
+                      });
+                    },
                     style: ElevatedButton.styleFrom(
-                      primary: Color(0xffE4E9FF),
+                      primary: isAloneSelected
+                          ? Color(0xff3B4866)
+                          : Color(0xffE4E9FF),
                       onPrimary: Color(0xff3B4866),
                       minimumSize: Size(171.48, 102),
                       shape: RoundedRectangleBorder(
@@ -83,7 +99,11 @@ class _InputScreenState extends State<InputScreen> {
                         Text(
                           '나 혼자 참여하기',
                           style: TextStyle(
-                              fontSize: 14, fontFamily: 'moebiusRegular'),
+                              fontSize: 14,
+                              fontFamily: 'moebiusRegular',
+                              color: isAloneSelected
+                                  ? Colors.white
+                                  : Color(0xff3B4866)), // 글자색 변경),
                         ),
                         SizedBox(height: 16),
                         SvgPicture.asset(
@@ -98,9 +118,15 @@ class _InputScreenState extends State<InputScreen> {
                   height: 102,
                   width: 171.48,
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        isParticipateSelected = !isParticipateSelected;
+                      });
+                    },
                     style: ElevatedButton.styleFrom(
-                      primary: Color(0xffE4E9FF),
+                      primary: isParticipateSelected
+                          ? Color(0xff3B4866)
+                          : Color(0xffE4E9FF),
                       onPrimary: Color(0xff3B4866),
                       minimumSize: Size(171.48, 102),
                       shape: RoundedRectangleBorder(
@@ -113,7 +139,11 @@ class _InputScreenState extends State<InputScreen> {
                         Text(
                           '친구와 같이 참여하기',
                           style: TextStyle(
-                              fontSize: 14, fontFamily: 'moebiusRegular'),
+                              fontSize: 14,
+                              fontFamily: 'moebiusRegular',
+                              color: isParticipateSelected
+                                  ? Colors.white
+                                  : Color(0xff3B4866)), // 숫자 색 변경), // 글자색 변경),
                         ),
                         SizedBox(height: 8),
                         Row(
@@ -149,10 +179,11 @@ class _InputScreenState extends State<InputScreen> {
                             Text(
                               '$friendsCount',
                               style: TextStyle(
-                                fontSize: 12,
-                                fontFamily: 'moebiusBold',
-                                color: Color(0xff3B4866),
-                              ),
+                                  fontSize: 12,
+                                  fontFamily: 'moebiusBold',
+                                  color: isParticipateSelected
+                                      ? Colors.white
+                                      : Color(0xff3B4866)), // 숫자 색 변경
                             ),
                             SizedBox(width: 4),
                             ElevatedButton(
@@ -176,14 +207,20 @@ class _InputScreenState extends State<InputScreen> {
               ],
             ),
           ),
-          SizedBox(height: 30), // Spacing between buttons and "참여하기" button
+          SizedBox(height: 30),
           SizedBox(
             width: 358,
             height: 51,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                if ((isAloneSelected || isParticipateSelected)) {
+                  navigateToChoiceScreen();
+                }
+              },
               style: ElevatedButton.styleFrom(
-                primary: Color(0xff727DBC),
+                primary: ((isAloneSelected || isParticipateSelected))
+                    ? Color(0xff727DBC)
+                    : Colors.grey,
                 onPrimary: Colors.white,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
@@ -197,7 +234,6 @@ class _InputScreenState extends State<InputScreen> {
           ),
         ],
       ),
-      //bottomNavigationBar: Bottom(), // error reason
     );
   }
 }
