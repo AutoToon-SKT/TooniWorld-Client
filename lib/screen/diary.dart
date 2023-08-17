aa63fc3d2e5d8b4cf2e5d5c887430097ae137f76
ohjungmin <jmin980317@naver.com>
2023-08-17 09:59:00 +0900

    ADD : 이야기 작성 스크린

---
+++ lib/screen/diary.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      home: DiaryScreen(),
+    );
+  }
+}
+
+class DiaryScreen extends StatefulWidget {
+  @override
+  _DiaryScreenState createState() => _DiaryScreenState();
+}
+
+class _DiaryScreenState extends State<DiaryScreen> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100.0),
+        child: AppBar(
+          backgroundColor: Color(0xffF1F2F6),
+          elevation: 0,
+          flexibleSpace: Column(
+            children: [
+              SizedBox(height: 28.0),
+              SvgPicture.asset(
+                'assets/icons/input/logo.svg',
+                height: 95,
+              ),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: Color(0xffF1F2F6),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 27.0),
+              Text(
+                '당신의 이야기를 작성해 볼까요?',
+                style: TextStyle(
+                  color: Color(0xff3B4866),
+                  fontSize: 24,
+                  fontFamily: 'moebiusRegular',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 24),
+              Text(
+                '이야기 작성하기 (Write your Story)',
+                style: TextStyle(
+                  color: Color(0xff3B4866),
+                  fontSize: 16,
+                  fontFamily: 'moebiusBold',
+                ),
+              ),
+              SizedBox(height: 8),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
